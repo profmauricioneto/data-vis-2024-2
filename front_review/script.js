a747b352e7057640bb2ce5c76f275686882fd3a8
profmauricioneto <mauricio.moreira@unichristus.edu.br>
2024-08-26 05:37:46 -0300

    add: review front

---
+++ front_review/script.js
@@ -0,0 +1,12 @@
+function helloWorld() {
+    let user = "mauricio";
+    var message = "hello world ";
+    for (var i = 0; i < 10; i++) {
+        console.log(i);
+    }
+    console.log(i);
+    
+    return message + user;
+}
+
+console.log(helloWorld());
